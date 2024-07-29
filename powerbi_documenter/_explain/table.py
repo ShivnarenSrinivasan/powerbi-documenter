d2ffbea7e3d4f569210dfaf0f3a4ecf7b867d79e
srinivasan <shivnaren@gmail.com>
2024-07-29 18:58:58 +0530

    Tweak prompt to simplify explanation.

--- powerbi_documenter/_explain/table.py
+++ powerbi_documenter/_explain/table.py
@@ -8,6 +8,7 @@ from langchain_openai import AzureChatOpenAI
 
 _SYS_MESSAGE = '''
 You are a helpful technical assistant capable of explaining code and queries to non-technical people.
+In your entire response, use as simple english as you can.
 
 Below is the contents of powerbi tmdl file for a table.
 Ignore everything other than the `partition` section
@@ -22,20 +23,21 @@ Please explain the query in the source expression in detail in the following for
 <column names of passthrough columns that are simple selects>
 
 ### Filters Applied
-<filters applied>
+<filters applied, except filters on `dbt_valid_to`. Convert SQL-like logic to simple English
+Example:
+    - instead of "accntg_doc_item_gl_nbr IN ('0106040104', '0106040105')", say
+    "accntg_doc_item_gl_nbr is either '0106040104' OR '0106040105'
+>
 
 ### Calculated Columns
 <computed columns that are created in the query>
+<if the query creates a literal table, display the data in a formatted table>
 
 <Don't mention anything about data type conversion.>
-
-## Source Code
-<include the `partition` section here>
 '''
 
 
 def explain(table_definition: str, llm: AzureChatOpenAI) -> str:
-
     human_prompt = '''
     text: {text}
     '''
