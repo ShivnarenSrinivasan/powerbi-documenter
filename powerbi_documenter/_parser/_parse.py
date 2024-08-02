5588dd45f452f951604830a2c94362f17e243aba
srinivasan <shivnaren@gmail.com>
2024-08-02 14:02:49 +0530

    Add common parsing utilities.

---
+++ powerbi_documenter/_parser/_parse.py
@@ -0,0 +1,20 @@
+"""Common parsing utilities."""
+from collections.abc import Iterable
+import re
+from typing import Literal as L
+
+
+def body_fields(text: Iterable[str]) -> dict[str, str]:
+    return dict(extract_body_field(line) for line in text)
+
+
+def extract_body_field(row: str) -> tuple[str, str]:
+    name, *rest = row.split()
+    return name.replace(':', ''), ' '.join(rest)
+
+
+def extract_field_name(field: str, field_type: L['measure', 'column']) -> str:
+    mat = re.search(f"{field_type} (.+)? =", field)
+    assert mat is not None
+    removed_quotes = mat.group(1).replace("'", '')
+    return removed_quotes
